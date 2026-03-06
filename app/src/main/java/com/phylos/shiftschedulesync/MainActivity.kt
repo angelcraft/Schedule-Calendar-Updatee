bd2e1b82a63e500277d42f4fbf120f94024eb668
Angetoumer <angellazaroramirez@gmail.com>
2026-03-06 22:17:58 +0100

    Add files via upload

---
+++ app/src/main/java/com/phylos/shiftschedulesync/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.phylos.shiftschedulesync
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            App()
+        }
+    }
+}
+
+@Composable
+fun App() {
+    MaterialTheme {
+        Surface(modifier = Modifier) {
+            Text("ShiftScheduleSync base project ready 🚀")
+        }
+    }
+}
