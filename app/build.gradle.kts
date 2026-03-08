9f44c13c8cb09d9bc9a5e9b7c4b2df7c4fa57e09
Angetoumer <angellazaroramirez@gmail.com>
2026-03-08 08:39:01 +0100

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,4 +47,12 @@ dependencies {
     implementation("androidx.datastore:datastore-preferences:1.1.1")
     implementation("com.google.mlkit:text-recognition:16.0.1")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.8.1")
+ implementation("androidx.activity:activity-compose:1.9.0")
+ implementation("androidx.compose.ui:ui:1.6.8")
+    implementation("androidx.compose.material3:material3:1.2.1")
+    implementation("androidx.compose.ui:ui-tooling-preview:1.6.8")
+
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.0")
+
+    implementation("com.google.mlkit:text-recognition:16.0.0")
 }
